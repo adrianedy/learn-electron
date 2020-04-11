75b3137952d4fae6da2d01da9533c8e24ef9aa02
Adrian Edy <adrianedypratama@gmail.com>
2020-04-11 23:13:16 +0800

    Electron part 6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const electron = require('electron')
+const electron  = require('electron')
+const uuid      = require('uuid').v4
 
 const {app, BrowserWindow, Menu, ipcMain} = electron
 
@@ -6,6 +7,7 @@ let todayWindow
 let createWindow
 let listWindow
 let aboutWindow
+let allAppointment = []
 
 app.on('ready', () => {
     todayWindow = new BrowserWindow({
@@ -78,7 +80,13 @@ const aboutWindowCreator = () => {
 
 
 ipcMain.on('appointment:create', (event, appointment) => {
-    console.log(appointment)
+    appointment['id']   = uuid()
+    appointment['done'] = 0
+    allAppointment.push(appointment)
+
+    createWindow.close()
+    
+    console.log(allAppointment)
 })
 
 ipcMain.on('appointment:request:list', event => {
