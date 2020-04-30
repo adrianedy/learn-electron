c613cbe2b7538248b1b802d13797b00a01b9c8a7
Adrian Edy <adrianedypratama@gmail.com>
2020-04-30 22:03:24 +0800

    Electron part 10

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const electron  = require('electron')
 const uuid      = require('uuid').v4
+const fs        = require('fs')
 
 const {app, BrowserWindow, Menu, ipcMain} = electron
 
@@ -8,6 +9,13 @@ let createWindow
 let listWindow
 let aboutWindow
 let allAppointment = []
+fs.readFile("db.json", (err, jsonAppointment) => {
+    if(!err)
+    {
+        const oldAppointment = JSON.parse(jsonAppointment)
+        allAppointment = oldAppointment
+    }
+})
 
 app.on('ready', () => {
     todayWindow = new BrowserWindow({
@@ -19,6 +27,10 @@ app.on('ready', () => {
 
     todayWindow.loadURL(`file://${__dirname}/today.html`)
     todayWindow.on('closed', () => {
+
+        const jsonAppointment = JSON.stringify(allAppointment)
+        fs.writeFileSync('db.json', jsonAppointment)
+
         app.quit()
         todayWindow = null
     })
